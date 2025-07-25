8d4fcf35b1e01a87052214cf27a85c3dfbce5f14
Yacquub <b_yacquub@hotmail.co.uk>
2025-07-25 15:39:18 +0100

    attempting second

--- days_api/app.py
+++ days_api/app.py
@@ -22,6 +22,7 @@ def add_to_history(current_request):
         "route": current_request.endpoint
     })
 
+
 def clear_history():
     """Clears the app history."""
     app_history.clear()
@@ -31,6 +32,26 @@ def clear_history():
 def index():
     """Returns an API welcome messsage."""
     return jsonify({"message": "Welcome to the Days API."})
+
+
+@app.route("/between", methods=["POST"])
+def between():
+    if request.method == "POST":
+        data = request.json
+        if len(data) != 2:
+            return {"error": "Missing required data."}, 400
+        required = ['first', 'last']
+        for key in data.keys():
+            if key not in required:
+                return {"error": "Missing required data."}, 400
+        try:
+            first = datetime.strptime(data[0], '%d.%m.%Y')
+        except ValueError:
+            return {
+                "error": "Unable to convert value to datetime."
+            }
+
+        return get_days_between(first, second), 200
 
 
 if __name__ == "__main__":
